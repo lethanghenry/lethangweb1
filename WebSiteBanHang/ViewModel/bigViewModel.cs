9007b6fdd67cc61b77e77548499de192de073006
lethanghenry <lethang2kth@gmail.com>
2021-12-03 18:08:16 +0700

    first commit

---
+++ WebSiteBanHang/ViewModel/bigViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebSiteBanHang.Models;
+
+namespace WebSiteBanHang.ViewModel
+{
+    public class bigViewModel
+    {
+        public Category Category { get; set; }
+        public Product Product { get; set; }
+        public UserProduct UserProduct { get; set; }
+    }
+}
